26ac10a008d871079d2718a54111814a71b1c529
antonkulaga <antonkulaga@gmail.com>
2017-07-24 01:11:31 +0300

    config reorganization

--- index.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -p 9999:9999 -p 4040:4040 --net=bigdatadocker_default -v /pipelines/bigdata-docker:/data quay.io/comp-bio-aging/adam:latest --master spark://spark-master:7077 --conf spark.driver.port=9999 --executor-memory 21G --total-executor-cores 8 -- fasta2adam hdfs://namenode:8020/genomes/HUMAN/26/GRCh38.p10.genome.fa.gz hdfs://namenode:8020/genomes/HUMAN/26/genome.adam -fragment_length 100000
