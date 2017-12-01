14470ff1b0efa20519425f76ec7ccf87324601fc
antonkulaga <antonkulaga@gmail.com>
2017-12-02 01:09:42 +0200

    small config fix for notebook

--- run-swarm.sh
+++ run-swarm.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-docker network create -d overlay --attachable core
-docker stack deploy -c hadoop-swarm.yml hadoop
-docker stack deploy -c spark-swarm.yml hadoop
+#docker network create -d overlay --attachable core
+docker stack deploy -c bigdata-swarm.yml bigdata
