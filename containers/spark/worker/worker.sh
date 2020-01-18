4b4f47c9c416cc164a65ce6c6da28d9b6669423a
antonkulaga <antonkulaga@gmail.com>
2020-01-18 02:47:01 +0200

    trying to fix worker

--- containers/spark/worker/worker.sh
+++ containers/spark/worker/worker.sh
@@ -10,6 +10,6 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
-/spark/sbin/start-shuffle-service.sh
+#/spark/sbin/start-shuffle-service.sh
 
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
